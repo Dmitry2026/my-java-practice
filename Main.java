0d4ea721b2bf62b9dfa327a17053da630f536448
Dmitry2026 <dimasik.totsamyy.02@mail.ru>
2026-06-18 00:07:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,43 @@
+import java. util.ArrayList;
 import java.util.Scanner;
-public class Main{
-    public static void main(String[]args){
+class TimeLong{
+    int h;
+    int m;
+    public TimeLong(int hors, int minutes){
+        this.h=hors;
+        this.m=minutes;
+    }
+    public boolean checkMirror(){
+        return h==m;
+    }
+}
+class Maim{
+    public static void main( String[]args){
         Scanner scanner=new Scanner(System.in);
-        while (true) {
-            System.out.println(" Введите часы:");
-            int hours = scanner.nextInt();
-            System.out.println("Введите минуты:");
-            int minutes = scanner.nextInt();
-            if (hours == minutes) {
-                System.out.println("Синхронизация время зеркальное");
-            } else {
-                System.out.println("Обычное время");
+        ArrayList<TimeLong>list=new ArrayList<>();
+        while (true){
+            System.out.println("Введите часы( или 99 для выхода:)");
+            int hors=scanner.nextInt();
+            if(hors==99) {
+                break;
             }
+                System.out.println("ВВедите минуты:");
+            int minutes =scanner.nextInt();
+            TimeLong log=new TimeLong(hors,minutes);
+            list.add(log);
+            if(log.checkMirror() ){
+                System.out.println("Синхронизация зафиксирована в объекте\n");
+
+            }else {
+                System.out.println("Обычный заход сохранен\n");
+            }
+        }
+        System.out.println("\n  Сессия завершена . Проверяем базу данных...");
+        if (list.isEmpty()){
+            System.out.println("База пуста. Вы ничего не ввели.");
+        }else {
+            System.out.println("Всего заходов в списке "+ list.size());
+            System.out.println("Индексы проверены. Код готов к депою");
         }
     }
 }
