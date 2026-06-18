a491e2fe67685f6732c074f6f20f2874e2ce80a6
Dmitry2026 <dimasik.totsamyy.02@mail.ru>
2026-06-18 20:14:32 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,29 @@
-import java. util.ArrayList;
-import java.util.Scanner;
-class TimeLong{
-    int h;
-    int m;
-    public TimeLong(int hors, int minutes){
-        this.h=hors;
-        this.m=minutes;
-    }
-    public boolean checkMirror(){
-        return h==m;
-    }
-}
-class Maim{
-    public static void main( String[]args){
-        Scanner scanner=new Scanner(System.in);
-        ArrayList<TimeLong>list=new ArrayList<>();
-        while (true){
-            System.out.println("Введите часы( или 99 для выхода:)");
-            int hors=scanner.nextInt();
-            if(hors==99) {
+import java.util.ArrayList;
+import java. util. Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Double> transactions = new ArrayList<>();
+        System.out.println("<<<Система Учета Расходов>>>");
+        System.out.println("Введите сумму расходов(или напишите 'exit'  для завершения):");
+        while (true) {
+            System.out.println("Введите сумму");
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
                 break;
             }
-                System.out.println("ВВедите минуты:");
-            int minutes =scanner.nextInt();
-            TimeLong log=new TimeLong(hors,minutes);
-            list.add(log);
-            if(log.checkMirror() ){
-                System.out.println("Синхронизация зафиксирована в объекте\n");
+            double amount = Double.parseDouble(input);
+            transactions.add(amount);
+            System.out.println("Транзакция на сумму " + amount + " успешно сохранена");
 
-            }else {
-                System.out.println("Обычный заход сохранен\n");
+
+            System.out.println("\n <<<Сессия Завершена>>>");
+            System.out.println("Всего совершенных операций:" + transactions.size());
+            double totalSum = 0;
+            for (double t : transactions) {
+                totalSum += t;
             }
-        }
-        System.out.println("\n  Сессия завершена . Проверяем базу данных...");
-        if (list.isEmpty()){
-            System.out.println("База пуста. Вы ничего не ввели.");
-        }else {
-            System.out.println("Всего заходов в списке "+ list.size());
-            System.out.println("Индексы проверены. Код готов к депою");
+            System.out.println("Общая сумма расходов:" + totalSum + "руб.");
         }
     }
 }
