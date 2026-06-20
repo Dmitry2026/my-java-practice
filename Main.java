fa4ec735774c584d8297b98139828b4fe3882b8f
Dmitry2026 <dimasik.totsamyy.02@mail.ru>
2026-06-20 22:45:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,48 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java. util. Scanner;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Double> transactions = new ArrayList<>();
-        System.out.println("<<<Система Учета Расходов>>>");
-        System.out.println("Введите сумму расходов(или напишите 'exit'  для завершения):");
+        ArrayList<Transaction> history = new ArrayList<>();
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+
+        System.out.println("<<<Т-Система: Логистики транзикций>>>");
+
         while (true) {
-            System.out.println("Введите сумму");
-            String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("exit")) {
-                break;
+            System.out.println("Катерогория ( или 'exit'): ");
+            String cat = scanner.nextLine();
+
+            if (cat.equalsIgnoreCase("exit")) break;
+
+            System.out.println("Сумма: ");
+            String sumStr = scanner.nextLine().replace(',', '.').trim();
+
+            try {
+                BigDecimal money = new BigDecimal(sumStr).setScale(2, RoundingMode.HALF_UP);
+                Transaction th = new Transaction(cat, money);
+                history.add(th);
+                System.out.println("Объек Transaction успешно добавлен в память.");
+            } catch (NumberFormatException | ArithmeticException e) {
+                System.out.println("Ошибка парсинга суммы. Попробуйте еще раз.");
+                System.out.println();
             }
-            double amount = Double.parseDouble(input);
-            transactions.add(amount);
-            System.out.println("Транзакция на сумму " + amount + " успешно сохранена");
 
 
-            System.out.println("\n <<<Сессия Завершена>>>");
-            System.out.println("Всего совершенных операций:" + transactions.size());
-            double totalSum = 0;
-            for (double t : transactions) {
-                totalSum += t;
+            System.out.println("\n<<<Выгрузка транзикций из сети>>>");
+            BigDecimal total = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP);
+
+            for (Transaction t : history) {
+                System.out.println("Транзикция >>> Категория: " + t.getCategory() + " | Сумма: " + df.format(t.getAmount()) + " руб.");
+                total = total.add(t.getAmount());
             }
-            System.out.println("Общая сумма расходов:" + totalSum + "руб.");
+
+            System.out.println("Итого обработанона сумму: " + df.format(total) + " руб.");
+
         }
+        scanner.close();
     }
 }
